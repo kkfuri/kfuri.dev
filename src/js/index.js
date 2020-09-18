123e554f807dc642c7b7c271e9ab6b59fd0e2261
Diego Kfuri <kkfuri@gmail.com>
2020-09-18 19:35:03 -0300

    fix: adjust identation at console

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,12 @@
 window.onload = () => {
   console.log("%c ", "padding: 400px");
-  console.log(
-    `
-     _     _
+  console.log(`     _     _
     | |   (_)
     | |__  _ _ __ ___   _ __ ___   ___
     | '_ \\| | '__/ _ \\ | '_ ' _ \\ / _ \\
     | | | | | | |  __/ | | | | | |  __/
-    |_| |_|_|_|  \\___| |_| |_| |_|\\___|`
-  );
+    |_| |_|_|_|  \\___| |_| |_| |_|\\___|
+    `);
   console.log(
     `%c contact email: kkfuri@gmail.com`,
     "color: #ffd000; padding: 12px 40px; background-color: black;"
