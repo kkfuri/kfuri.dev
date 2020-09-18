e78f9c102d34ea0eadcd2f852a8d7290e62e7daa
Diego Kfuri <kkfuri@gmail.com>
2020-09-18 19:33:26 -0300

    feat: add contact email at console

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,17 @@
 window.onload = () => {
+  console.log("%c ", "padding: 400px");
+  console.log(
+    `
+     _     _
+    | |   (_)
+    | |__  _ _ __ ___   _ __ ___   ___
+    | '_ \\| | '__/ _ \\ | '_ ' _ \\ / _ \\
+    | | | | | | |  __/ | | | | | |  __/
+    |_| |_|_|_|  \\___| |_| |_| |_|\\___|`
+  );
+  console.log(
+    `%c contact email: kkfuri@gmail.com`,
+    "color: #ffd000; padding: 12px 40px; background-color: black;"
+  );
   setTimeout(() => (document.getElementById("card").className = "show"), 400);
 };
