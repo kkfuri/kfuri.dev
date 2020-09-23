f6b08ac3382e8927fc8191892ab87217ba320f22
Diego Kfuri <kkfuri@gmail.com>
2020-09-23 10:53:39 -0300

    feat: update projects order

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,50 +1,6 @@
 export default [
   {
     id: 1,
-    name: "Covid no Brasil",
-    placeholder:
-      "https://github.com/kkfuri/svelte-covid/raw/master/.github/covid.gif?raw=true",
-    repo: "https://github.com/kkfuri/svelte-covid",
-    link: "https://covid-no-brasil.now.sh/",
-    description: `A project designed to
-    <strong>deliver information</strong> about the spread of
-    COVID-19 in Brazilian states.
-    <br />
-    Developed with <strong>Svelte</strong> using
-    <strong>Sapper</strong>, a framework for building
-    high-performance web apps. All the styling was made using the
-    Svelte style scoped block, that is, <strong>pure CSS</strong>.
-    The data is obtained from an public API made by brazilians.
-    The deploy was done using <strong>Vercel</strong>.`,
-    tech: ["Svelte", "Sapper", "CSS", "Axios", "Vercel", "Javascript"],
-  },
-  {
-    id: 2,
-    name: "Superheroes",
-    placeholder:
-      "https://github.com/kkfuri/superheroes/blob/master/.github/superheroes.gif?raw=true",
-    repo: "https://github.com/kkfuri/superheroes",
-    link: "https://superheroes.now.sh/",
-    description: `A project built with the intention of learning more about
-    <strong>Next.js</strong>, <strong>TailwindCSS</strong> and
-    creating a good experience for users. <br />
-    Developed with React using Next.js. The whole style was done
-    using the tailwind classes. The animation was built with
-    <strong>framer motion</strong>. The implantation was done
-    using <strong>Vercel</strong>. The data is obtained from an
-    API called superheroapi.`,
-    tech: [
-      "React",
-      "Next.js",
-      "TailwindCSS",
-      "Vercel",
-      "Axios",
-      "Framer Motion",
-      "Javascript",
-    ],
-  },
-  {
-    id: 3,
     name: "Pop Blog",
     placeholder:
       "https://github.com/kkfuri/gatsby-anime-blog/raw/master/.github/page.gif?raw=true",
@@ -75,6 +31,50 @@ export default [
       "Contentful",
       "Typescript",
       "Vercel",
+    ],
+  },
+  {
+    id: 2,
+    name: "Covid no Brasil",
+    placeholder:
+      "https://github.com/kkfuri/svelte-covid/raw/master/.github/covid.gif?raw=true",
+    repo: "https://github.com/kkfuri/svelte-covid",
+    link: "https://covid-no-brasil.now.sh/",
+    description: `A project designed to
+    <strong>deliver information</strong> about the spread of
+    COVID-19 in Brazilian states.
+    <br />
+    Developed with <strong>Svelte</strong> using
+    <strong>Sapper</strong>, a framework for building
+    high-performance web apps. All the styling was made using the
+    Svelte style scoped block, that is, <strong>pure CSS</strong>.
+    The data is obtained from an public API made by brazilians.
+    The deploy was done using <strong>Vercel</strong>.`,
+    tech: ["Svelte", "Sapper", "CSS", "Axios", "Vercel", "Javascript"],
+  },
+  {
+    id: 3,
+    name: "Superheroes",
+    placeholder:
+      "https://github.com/kkfuri/superheroes/raw/master/.github/superheroes.gif?raw=true",
+    repo: "https://github.com/kkfuri/superheroes",
+    link: "https://superheroes.now.sh/",
+    description: `A project built with the intention of learning more about
+    <strong>Next.js</strong>, <strong>TailwindCSS</strong> and
+    creating a good experience for users. <br />
+    Developed with React using Next.js. The whole style was done
+    using the tailwind classes. The animation was built with
+    <strong>framer motion</strong>. The implantation was done
+    using <strong>Vercel</strong>. The data is obtained from an
+    API called superheroapi.`,
+    tech: [
+      "React",
+      "Next.js",
+      "TailwindCSS",
+      "Vercel",
+      "Axios",
+      "Framer Motion",
+      "Javascript",
     ],
   },
 ];
