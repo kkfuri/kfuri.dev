5aae55553405450ecd1ba4b6b0d04bad8f1c4211
kkfuri <kkfuri@gmail.com>
2020-11-25 10:23:25 -0300

    feat: add responsiveness to presentation

--- src/containers/presentation.js
+++ src/containers/presentation.js
@@ -6,30 +6,42 @@ export default function Presentation() {
     <div className="container flex flex-col items-center justify-center min-h-screen mx-auto">
       <div className="w-full max-w-2xl p-1 mx-auto border border-red-400 shadow-sm-white">
         <div className="flex space-x-4">
-          <div className="bg-white border border-gray200">
+          <div className="flex items-center bg-white border border-gray200">
             <Image
               src="https://avatars0.githubusercontent.com/u/42350846"
               width={460}
               height={460}
             />
           </div>
-          <div className="flex flex-col justify-between w-full py-4">
+          <div className="flex flex-col justify-between w-full py-1 md:py-4">
             <div className="flex-1">
-              <h1 className="font-sans text-6xl font-semibold leading-snug">
+              <h1 className="font-sans text-3xl font-semibold lg:leading-snug md:text-6xl">
                 Diego Kfuri
               </h1>
-              <StyledText className="tracking-wider">
+              <StyledText className="text-sm tracking-wider md:text-xl md:mt-2">
                 software engineer
               </StyledText>
             </div>
-            <ul>
-              <StyledText separator="using" as="li">
+            <ul className="lg:mt-6">
+              <StyledText
+                separator="using"
+                as="li"
+                className="text-xs sm:text-sm lg:text-base"
+              >
                 working with front-end using mainly next.js
               </StyledText>
-              <StyledText separator="&" as="li">
+              <StyledText
+                separator="&"
+                as="li"
+                className="text-xs sm:text-sm lg:text-base"
+              >
                 learning typescript & nodejs
               </StyledText>
-              <StyledText separator="&" as="li">
+              <StyledText
+                separator="&"
+                as="li"
+                className="text-xs sm:text-sm lg:text-base"
+              >
                 in love with design & games
               </StyledText>
             </ul>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,9 @@ import Projects from "@/containers/projects";
 
 export default function Home() {
   return (
-    <>
+    <div className="px-2">
       <Presentation />
       <Projects />
-    </>
+    </div>
   );
 }
